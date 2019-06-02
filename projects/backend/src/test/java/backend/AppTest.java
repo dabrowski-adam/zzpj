313531f93ebfa98bbe6fcdecb2bc4b512719c25d
Paweł Skowroński <p.skowronski@sharehire.pl>
2019-06-02 14:25:27 +0200

    Add spring web package, add IndexController with hello world

--- projects/backend/src/test/java/backend/AppTest.java
+++ projects/backend/src/test/java/backend/AppTest.java
@@ -3,18 +3,6 @@
  */
 package backend;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
 
-    @Test public void testAppShouldGreetWithHelloWorld() {
-        App classUnderTest = new App();
-        assertEquals("Hello world.", classUnderTest.getGreeting());
-        assertNotEquals("Bye world.", classUnderTest.getGreeting());
-    }
 }
