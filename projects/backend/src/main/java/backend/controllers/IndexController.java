50cbfbc4fea0c071568ee28fd250a04c86ae1a7a
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-06-03 01:57:33 +0200

    Add SubjectDTO to IndexController

--- projects/backend/src/main/java/backend/controllers/IndexController.java
+++ projects/backend/src/main/java/backend/controllers/IndexController.java
@@ -1,16 +1,27 @@
 package backend.controllers;
 
 import backend.dto.LectureDTO;
+import backend.dto.SubjectDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public ResponseEntity<LectureDTO> index() {
-        LectureDTO result = new LectureDTO();
+    public ResponseEntity<Map> index() {
+        LectureDTO lectureDTO = new LectureDTO();
+        SubjectDTO subjectDTO = new SubjectDTO();
+
+        Map<String, Object> result = Map.of(
+                "lecture", lectureDTO,
+                "subject", subjectDTO
+        );
+
         return ResponseEntity.ok(result);
     }
 }
