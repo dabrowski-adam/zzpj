588117dd14f30b5e8bedfff32e86331545a6e1b6
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-06-03 01:54:30 +0200

    Wire SubjectsController

--- projects/backend/src/main/java/backend/controllers/SubjectsController.java
+++ projects/backend/src/main/java/backend/controllers/SubjectsController.java
@@ -1,10 +1,23 @@
 package backend.controllers;
 
+import backend.service.SubjectService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("subjects")
 public class SubjectsController {
+
+    private final SubjectService subjectService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public SubjectsController(SubjectService subjectService, ModelMapper modelMapper) {
+        this.subjectService = subjectService;
+        this.modelMapper = modelMapper;
+    }
+
     @PostMapping("add")
     public String signup() {
         return "Subject Add Endpoint";
