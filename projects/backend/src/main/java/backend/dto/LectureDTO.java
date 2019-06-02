338e61c1ad7cbb74ed364844841e8fa6210555a2
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-06-02 18:13:58 +0200

    added LectureDTO

--- projects/backend/src/main/java/backend/dto/LectureDTO.java
+++ projects/backend/src/main/java/backend/dto/LectureDTO.java
@@ -1,4 +1,18 @@
 package backend.dto;
 
-public class LectureDTO {
+import backend.domain.Lecture;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class LectureDTO implements Serializable {
+    //TODO: Add remaining data
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String key;
+    private long pin;
+    private boolean isOpen;
+    private boolean isChecked;
 }
