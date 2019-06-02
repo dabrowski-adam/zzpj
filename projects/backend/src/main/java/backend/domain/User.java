062b42088dbb9124c81932ceacb27300fd274d17
Michał Grzelak <grzelakm@outlook.com>
2019-06-02 18:51:33 +0200

    add @Id annotation to User id field

--- projects/backend/src/main/java/backend/domain/User.java
+++ projects/backend/src/main/java/backend/domain/User.java
@@ -1,12 +1,14 @@
 package backend.domain;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.time.LocalDateTime;
 
 @Data
 public class User {
+    @Id
     private String id;
     private String name;
     private String surname;
