439b1fdf459c9a80426a4cb08d90f6d0e232251a
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-06-02 18:13:33 +0200

    spring config class

--- projects/backend/src/main/java/backend/config/AppConfig.java
+++ projects/backend/src/main/java/backend/config/AppConfig.java
@@ -1,4 +1,15 @@
 package backend.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 }
