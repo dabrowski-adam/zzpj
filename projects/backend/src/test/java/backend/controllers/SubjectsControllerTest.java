31c8a0a5f6bee7a4254d29e14b9226b0ccb389b0
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-06-04 03:02:43 +0200

    Refactor

--- projects/backend/src/test/java/backend/controllers/SubjectsControllerTest.java
+++ projects/backend/src/test/java/backend/controllers/SubjectsControllerTest.java
@@ -4,12 +4,10 @@ import backend.domain.Subject;
 import backend.service.SubjectService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
