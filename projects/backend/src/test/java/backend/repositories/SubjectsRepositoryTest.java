f8bce2e95bc2e789d94dc1b53cb5d643b047f832
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-06-04 03:08:23 +0200

    Refactor

--- projects/backend/src/test/java/backend/repositories/SubjectsRepositoryTest.java
+++ projects/backend/src/test/java/backend/repositories/SubjectsRepositoryTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.*;
