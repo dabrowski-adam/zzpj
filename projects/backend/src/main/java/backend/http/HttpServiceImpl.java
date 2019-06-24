b3404f2a3bdecc1fb1c91049dc520c241321c8d1
Michał Grzelak <grzelakm@outlook.com>
2019-06-24 23:16:19 +0200

    fix checkstyle v2

--- projects/backend/src/main/java/backend/http/HttpServiceImpl.java
+++ projects/backend/src/main/java/backend/http/HttpServiceImpl.java
@@ -1,6 +1,11 @@
 package backend.http;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
