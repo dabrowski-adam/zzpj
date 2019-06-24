13afca883cfb6f1fd3ee75e22cd34b331748010e
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-06-25 00:12:35 +0200

    RandomLectureKeyGenerator util fix

--- projects/backend/src/main/java/backend/utils/RandomLectureKeyGenerator.java
+++ projects/backend/src/main/java/backend/utils/RandomLectureKeyGenerator.java
@@ -5,8 +5,16 @@ import java.util.Random;
 
 public class RandomLectureKeyGenerator {
   public static String generate() {
-    byte[] array = new byte[8];
-    new Random().nextBytes(array);
-    return new String(array, Charset.forName("UTF-8"));
+    int leftLimit = 97;
+    int rightLimit = 122;
+    int targetStringLength = 8;
+    Random random = new Random();
+    StringBuilder buffer = new StringBuilder(targetStringLength);
+    for (int i = 0; i < targetStringLength; i++) {
+      int randomLimitedInt = leftLimit + (int)
+        (random.nextFloat() * (rightLimit - leftLimit + 1));
+      buffer.append((char) randomLimitedInt);
+    }
+    return buffer.toString();
   }
 }
